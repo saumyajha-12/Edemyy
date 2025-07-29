4f3d393481299b9d365f338456f441d7d30c3350
saumyajha-12 <167021325+saumyajha-12@users.noreply.github.com>
2025-07-29 08:04:12 +0530

    fixed some image issues

--- client/src/components/SocialIcons.jsx
+++ client/src/components/SocialIcons.jsx
@@ -12,23 +12,17 @@ import {
 const SocialIcons = () => {
   return (
     <div className='flex items-center gap-3 mt-5 ml-1 mb-2 max-md:mt-4'>
-      <Link 
+     
+      {/* <Link 
         target='_blank' 
-        to='https://www.facebook.com/profile.php?id=100026766931684'
-        className="group transition transform hover:scale-110 text-[#0b16f1] hover:text-blue-500"
-      >
-        <FacebookLogo size={34} weight="fill" className="transition-colors duration-300" />
-      </Link>
-      <Link 
-        target='_blank' 
-        to='https://x.com/gps_96169'
+        to=''
         className="group transition transform hover:scale-110 text-[#cbc3c3] hover:text-gray-700"
       >
         <TwitterLogo size={34} weight="fill" className="transition-colors duration-300" />
-      </Link>
+      </Link> */}
       <Link 
         target='_blank' 
-        to='https://www.linkedin.com/public-profile/settings?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact-info%3BvqASsswPRlOsX3fpATCScA%3D%3D'
+        to='http://www.linkedin.com/in/saumya-prakash-085a01229'
         className="group transition transform hover:scale-110 text-[#1e17ea] hover:text-blue-600"
       >
         <LinkedinLogo size={34} weight="fill" className="transition-colors duration-300" />
