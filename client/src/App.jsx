014bfbcea032ee710efa1fdf3c7341dbee37661e
saumyajha-12 <spjha1207@gmail.com>
2026-08-15 17:19:05 +0530

    Implemented lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes, useMatch } from 'react-router-dom'
-import Home from './pages/student/Home'
-import CoursesList from './pages/student/CoursesList'
-import CourseDetails from './pages/student/CourseDetails'
-import MyEnrollMents from './pages/student/MyEnrollMents'
-import Player from './pages/student/Player'
-import Loading from './components/student/Loading'
-import Educator from './pages/educator/Educator'
-import Dashboard from './pages/educator/Dashboard'
-import AddCourse from './pages/educator/AddCourse'
-import MyCourses from './pages/educator/MyCourses'
-import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import Loading from './components/student/Loading'
 import "quill/dist/quill.snow.css";
 import { ToastContainer } from 'react-toastify';
-import About from './components/About'
-import ContactForm from './components/ContactForm'
 
+// Lazy loaded page components
+const Home = lazy(() => import('./pages/student/Home'))
+const CoursesList = lazy(() => import('./pages/student/CoursesList'))
+const CourseDetails = lazy(() => import('./pages/student/CourseDetails'))
+const MyEnrollMents = lazy(() => import('./pages/student/MyEnrollMents'))
+const Player = lazy(() => import('./pages/student/Player'))
+const Educator = lazy(() => import('./pages/educator/Educator'))
+const Dashboard = lazy(() => import('./pages/educator/Dashboard'))
+const AddCourse = lazy(() => import('./pages/educator/AddCourse'))
+const MyCourses = lazy(() => import('./pages/educator/MyCourses'))
+const StudentsEnrolled = lazy(() => import('./pages/educator/StudentsEnrolled'))
+const About = lazy(() => import('./components/About'))
+const ContactForm = lazy(() => import('./components/ContactForm'))
 
 const App = () => {
 
@@ -30,27 +31,29 @@ const App = () => {
       <ToastContainer />
       {!isEducatorRoute &&<Navbar/> }
       
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/course-list' element={<CoursesList/>} />
-        <Route path='/course-list/:input' element={<CoursesList/>} />
-        <Route path='/course/:id' element={<CourseDetails/>} />
-        <Route path='/my-enrollments' element={<MyEnrollMents/>} />
-        <Route path='/player/:courseId' element={<Player/>} />
-        <Route path='/loading/:path' element={<Loading/>} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/course-list' element={<CoursesList/>} />
+          <Route path='/course-list/:input' element={<CoursesList/>} />
+          <Route path='/course/:id' element={<CourseDetails/>} />
+          <Route path='/my-enrollments' element={<MyEnrollMents/>} />
+          <Route path='/player/:courseId' element={<Player/>} />
+          <Route path='/loading/:path' element={<Loading/>} />
 
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<ContactForm/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<ContactForm/>} />
 
 
-        <Route path='/educator' element={ <Educator />} >
-            <Route path='/educator' element={<Dashboard />} />
-            <Route path='add-course' element={<AddCourse />} />
-            <Route path='my-courses' element={<MyCourses />} />
-            <Route path='student-enrolled' element={<StudentsEnrolled />} />
-        </Route>
+          <Route path='/educator' element={ <Educator />} >
+              <Route path='/educator' element={<Dashboard />} />
+              <Route path='add-course' element={<AddCourse />} />
+              <Route path='my-courses' element={<MyCourses />} />
+              <Route path='student-enrolled' element={<StudentsEnrolled />} />
+          </Route>
 
-      </Routes>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
